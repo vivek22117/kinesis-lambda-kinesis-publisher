4b141030ea88b87b5dc57510569f4db05eadd50b
vivek22117 <vivekmishra22117@gmail.com>
2021-08-26 13:26:27 +0530

    adding test config and removing unwatned code form data tf file

--- aws-infra/lambda-resources/data.tf
+++ aws-infra/lambda-resources/data.tf
@@ -1,13 +1,12 @@
 ###################################################
 # Fetch remote state for S3 deployment bucket     #
 ###################################################
-data "terraform_remote_state" "backend" {
+data "terraform_remote_state" "s3_buckets" {
   backend = "s3"
 
   config = {
-    profile = var.profile
     bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
-    key     = "state/${var.environment}/aws/terraform.tfstate"
+    key     = "state/${var.environment}/s3-buckets/terraform.tfstate"
     region  = var.default_region
   }
 }
@@ -27,7 +26,6 @@ data "terraform_remote_state" "rsvp_subscriber_api" {
   backend = "s3"
 
   config = {
-    profile = var.profile
     bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
     key     = "state/${var.environment}/api-gateway/subscriber-api/terraform.tfstate"
     region  = var.default_region
